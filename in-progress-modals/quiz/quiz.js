4fc4cd13b7f5f1fc26df3ecf7b5b7b83e8c98168
rockyhenderson <jack@teamhendo.com>
2024-03-20 11:51:11 +0000

    candle data added and minro changes to code

--- in-progress-modals/quiz/quiz.js
+++ in-progress-modals/quiz/quiz.js
@@ -1,6 +1,6 @@
 function submitQuiz() {
-    
-    var userAnswers = []  
+
+    var userAnswers = []
 
     const reason = document.querySelector('input[name="reason"]:checked').value;
     userAnswers.push(reason);
@@ -12,22 +12,96 @@ function submitQuiz() {
     userAnswers.push(time);
 
     console.log(userAnswers)
-    
+
+    determinecandle(userAnswers)
+
 
 }
-function questiononenext(){
+function questiononenext() {
     document.getElementById("candlequizquestionone").style.display = "none";
     document.getElementById("candlequizquestiontwo").style.display = "block";
 }
-function questiontwonext(){
+function questiontwonext() {
     document.getElementById("candlequizquestiontwo").style.display = "none";
     document.getElementById("candlequizquestionthree").style.display = "block";
 }
-function questionthreenext(){
+function questionthreenext() {
     document.getElementById("candlequizquestionthree").style.display = "none";
     document.getElementById("candlequizquestionfour").style.display = "block";
 }
-function questionfournext(){
+function questionfournext() {
     document.getElementById("candlequizquestionfour").style.display = "none";
     submitQuiz()
 }
+
+function determinecandle(userAnswers) {
+
+    if (userAnswers[0] === "Relaxation" && userAnswers[1] === "Floral" && userAnswers[2] === "Bedroom" && userAnswers[3] === "Evening") {
+        alert("option 1");
+    }
+    if (userAnswers[0] === "Relaxation" && userAnswers[1] === "Floral" && userAnswers[2] === "Bedroom" && userAnswers[3] === "Late Night") {
+        alert("option 2");
+    }
+    if (userAnswers[0] === "Relaxation" && userAnswers[1] === "Floral" && userAnswers[2] === "Living Room" && userAnswers[3] === "Evening") {
+        alert("option 3");
+    }
+    if (userAnswers[0] === "Relaxation" && userAnswers[1] === "Floral" && userAnswers[2] === "Living Room" && userAnswers[3] === "Late Night") {
+        alert("option 4");
+    }
+    if (userAnswers[0] === "Relaxation" && userAnswers[1] === "Woody" && userAnswers[2] === "Bedroom" && userAnswers[3] === "Evening") {
+        alert("option 5");
+    }
+    if (userAnswers[0] === "Relaxation" && userAnswers[1] === "Woody" && userAnswers[2] === "Bedroom" && userAnswers[3] === "Late Night") {
+        alert("option 6");
+    }
+    if (userAnswers[0] === "Relaxation" && userAnswers[1] === "Woody" && userAnswers[2] === "Living Room" && userAnswers[3] === "Evening") {
+        alert("option 7");
+    }
+    if (userAnswers[0] === "Relaxation" && userAnswers[1] === "Woody" && userAnswers[2] === "Living Room" && userAnswers[3] === "Late Night") {
+        alert("option 8");
+    }
+    if (userAnswers[0] === "Decoration" && userAnswers[1] === "Floral" && userAnswers[2] === "Bedroom" && userAnswers[3] === "Evening") {
+        alert("option 9");
+    }
+    if (userAnswers[0] === "Decoration" && userAnswers[1] === "Floral" && userAnswers[2] === "Bedroom" && userAnswers[3] === "Late Night") {
+        alert("option 10");
+    }
+    if (userAnswers[0] === "Decoration" && userAnswers[1] === "Floral" && userAnswers[2] === "Living Room" && userAnswers[3] === "Evening") {
+        alert("option 11");
+    }
+    if (userAnswers[0] === "Decoration" && userAnswers[1] === "Floral" && userAnswers[2] === "Living Room" && userAnswers[3] === "Late Night") {
+        alert("option 12");
+    }
+    if (userAnswers[0] === "Decoration" && userAnswers[1] === "Woody" && userAnswers[2] === "Bedroom" && userAnswers[3] === "Evening") {
+        alert("option 13");
+    }
+    if (userAnswers[0] === "Decoration" && userAnswers[1] === "Woody" && userAnswers[2] === "Bedroom" && userAnswers[3] === "Late Night") {
+        alert("option 14");
+    }
+    if (userAnswers[0] === "Decoration" && userAnswers[1] === "Woody" && userAnswers[2] === "Living Room" && userAnswers[3] === "Evening") {
+        alert("option 15");
+    }
+    if (userAnswers[0] === "Decoration" && userAnswers[1] === "Woody" && userAnswers[2] === "Living Room" && userAnswers[3] === "Late Night") {
+        alert("option 16");
+    }
+
+
+
+
+}
+
+$(document).ready(function () {
+    $.ajax({
+        url: 'candles.json',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            products = data;
+            console.log(data);
+            //thank fuck this works
+        },
+        error: function (error) {
+            console.log('Error loading the JSON File')
+        }
+    })
+})
