26e0229338b76b2bfa794914de0d1e237b8ce8da
RockyhendersonPC <jack@teamhendo.com>
2024-05-01 15:08:26 +0100

    hello ;3

--- script.js
+++ script.js
@@ -7,6 +7,60 @@ var userSelections = {
     time: ''
 };
 
+// Array of image URLs
+const images = [
+    "url('img/mango/mango1.webp')",
+    "url('img/mango/mango2.webp')",
+    "url('img/mango/mango3.webp')",
+    "url('img/mango/mango4.webp')",
+    "url('img/mango/mango5.webp')"
+  ];
+  
+  let currentIndex = 0; // Start with the first image
+  
+  function changeBackgroundImage() {
+    const element = document.getElementById("product-image");
+    // Set the image
+    element.style.backgroundImage = images[currentIndex];
+    // Move to the next image index or loop back to the start
+    currentIndex = (currentIndex + 1) % images.length;
+  }
+  
+  // Set interval to change the background image every 5 seconds
+  setInterval(changeBackgroundImage, 5000);
+function changemango1(){
+    document.getElementById("product-image").style.backgroundImage = "url('img/mango/mango1.webp')";
+}
+function changemango2(){
+    document.getElementById("product-image").style.backgroundImage = "url('img/mango/mango2.webp')";
+}
+
+function changemango3(){
+    document.getElementById("product-image").style.backgroundImage = "url('img/mango/mango3.webp')";
+}
+
+function changemango4(){
+    document.getElementById("product-image").style.backgroundImage = "url('img/mango/mango4.webp')";
+}
+
+function changemango5(){
+    document.getElementById("product-image").style.backgroundImage = "url('img/mango/mango5.webp')";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
