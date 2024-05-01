ee6d1c2570483e237d3a9caeb2519b5a91143b20
RockyhendersonPC <jack@teamhendo.com>
2024-05-01 15:34:02 +0100

    push for ed

--- script.js
+++ script.js
@@ -7,27 +7,30 @@ var userSelections = {
     time: ''
 };
 
-// Array of image URLs
-const images = [
-    "url('img/mango/mango1.webp')",
-    "url('img/mango/mango2.webp')",
-    "url('img/mango/mango3.webp')",
-    "url('img/mango/mango4.webp')",
-    "url('img/mango/mango5.webp')"
-  ];
+// // Array of image URLs
+// const images = [
+//     "url('img/mango/mango1.webp')",
+//     "url('img/mango/mango2.webp')",
+//     "url('img/mango/mango3.webp')",
+//     "url('img/mango/mango4.webp')",
+//     "url('img/mango/mango5.webp')"
+//   ];
   
-  let currentIndex = 0; // Start with the first image
+//   let currentIndex = 0; // Start with the first image
   
-  function changeBackgroundImage() {
-    const element = document.getElementById("product-image");
-    // Set the image
-    element.style.backgroundImage = images[currentIndex];
-    // Move to the next image index or loop back to the start
-    currentIndex = (currentIndex + 1) % images.length;
-  }
+//   function changeBackgroundImage() {
+//     const element = document.getElementById("product-image");
+//     // Set the image
+//     element.style.backgroundImage = images[currentIndex];
+//     // Move to the next image index or loop back to the start
+//     currentIndex = (currentIndex + 1) % images.length;
+//   }
   
-  // Set interval to change the background image every 5 seconds
-  setInterval(changeBackgroundImage, 5000);
+//   // Set interval to change the background image every 5 seconds
+//   setInterval(changeBackgroundImage, 5000);
+
+
+
 function changemango1(){
     document.getElementById("product-image").style.backgroundImage = "url('img/mango/mango1.webp')";
 }
